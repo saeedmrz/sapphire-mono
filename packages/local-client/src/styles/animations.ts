99fede6b5f1273b9eda50e52da37052285adc408
unknown <saeedmirzaee@protonmail.com>
2022-10-10 02:54:38 -0700

    testing branch

--- packages/local-client/src/styles/animations.ts
+++ packages/local-client/src/styles/animations.ts
@@ -52,3 +52,13 @@ export const rotate = keyframes`
         transform: rotate(360deg);
     }
 `;
+
+export const rotatation = keyframes`
+    0% {
+        transform: rotate(0deg);
+    }
+
+    100% {
+        transform: rotate(360deg);
+    }
+`;
